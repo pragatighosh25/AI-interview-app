1af919455baef0af75726ed31f65a3e783201b12
Pragati Ghosh <pragati25ghosh@gmail.com>
2026-03-24 23:58:16 +0530

    fix css

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black">
       
-      <Card className="w-[350px] p-6">
+      <Card className="w-87.5 p-6">
         <CardContent className="flex flex-col gap-4">
 
           <h2 className="text-2xl font-bold text-center">Login</h2>
